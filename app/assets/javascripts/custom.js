fa0df33aa0fd98e8b9cde41dc507516d4c79aed9
Alex Hoover <hoover.alex@gmail.com>
2016-03-06 09:15:44 -0500

    Updated datepicker

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,4 +1,13 @@
+var datepicker = function() {
+	$( ".datepicker" ).datepicker({
+		dateFormat: "yy-mm-dd"
+	});
+};
+
 // This is a little fix that helps MDL cooperate with rails' Turbolinks
 document.addEventListener('page:change', function() {
   componentHandler.upgradeDom();
+  datepicker();
 });
+
+$(datepicker());
